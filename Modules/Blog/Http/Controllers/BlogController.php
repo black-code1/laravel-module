207690f0732043d96f661876a9c7db2cb7832414
black-code1 <legrandmahamat1@gmail.com>
2022-09-24 15:33:39 +0100

    Delete the specified module.

--- Modules/Blog/Http/Controllers/BlogController.php
+++ Modules/Blog/Http/Controllers/BlogController.php
@@ -16,7 +16,7 @@ class BlogController extends Controller
     public function index()
     {
         $module = Module::find('blog');
-        dd($module->enable());
+        $module->delete();
         return view('blog::index');
     }
 
