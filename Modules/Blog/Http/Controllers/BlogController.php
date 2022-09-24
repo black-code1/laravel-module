b394eb2751d0ea70116838371c2d1e5eb2ac5796
black-code1 <legrandmahamat1@gmail.com>
2022-09-24 15:35:48 +0100

    Get an array of module requirements.

--- Modules/Blog/Http/Controllers/BlogController.php
+++ Modules/Blog/Http/Controllers/BlogController.php
@@ -16,7 +16,7 @@ class BlogController extends Controller
     public function index()
     {
         $module = Module::find('blog');
-        $module->delete();
+        dd($module->getRequires());
         return view('blog::index');
     }
 
